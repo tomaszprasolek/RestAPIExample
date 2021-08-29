c97f9d860a5b7305e00c4c2c3f53e973ea8f4ef8
Tomasz Prasołek <prasol@o2.pl>
2021-08-29 06:18:05 +0200

    Add project files.

---
+++ RestAPIExample/BL/Database.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace RestAPIExample.BL
+{
+    public class Database
+    {
+        public List<Book> Books;
+
+        public Database()
+        {
+            Books = new List<Book>
+            {
+                new Book { Id = 1, Title = "Wiedźmin" },
+                new Book { Id = 2, Title = "Lupus" },
+                new Book { Id = 3, Title = "Narkonomia" }
+            };
+        }
+    }
+}
---
+++ RestAPIExample/Controllers/BooksController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestAPIExample.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPIExample.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private readonly ILogger<BooksController> _logger;
+        private readonly Database db;
+
+        public BooksController(ILogger<BooksController> logger)
+        {
+            _logger = logger;
+            db = new Database();
+        }
+
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return db.Books;
+        }
+
+        [HttpGet("book/{id}")]
+        public IActionResult Get(int id)
+        {
+            var book = db.Books.FirstOrDefault(x => x.Id == id);
+            if (book == null)
+                return NotFound();
+
+            return Ok(book);
+        }
+    }
+}
